f851f2b24cd65ccedbf3e228cfdd16f69b7cdcbe
Alain Metge <alain.metge@hotmail.fr>
2015-11-18 17:42:23 +0100

    fix build bug

--- build.cmd
+++ build.cmd
@@ -1,20 +1,20 @@
-dnvm use 1.0.0-beta8  
-dnu restore
+call dnvm use 1.0.0-beta8  
+call dnu restore
 if not "%errorlevel%"=="0" goto failure
 
 IF '%Configuration%' == '' (
-  dnu pack src\Jellyfish.Commands --configuration Release
-  dnu pack src\Jellyfish.Commands.vnext --configuration Release
+  call dnu pack src\Jellyfish.Commands --configuration Release
+  call dnu pack src\Jellyfish.Commands.vnext --configuration Release
 ) ELSE (
-  dnu pack src\Jellyfish.Commands --configuration %Configuration%
-  dnu pack src\Jellyfish.Commands.vnext --configuration %Configuration%
+  call dnu pack src\Jellyfish.Commands --configuration %Configuration%
+  call dnu pack src\Jellyfish.Commands.vnext --configuration %Configuration%
 )
 if not "%errorlevel%"=="0" goto failure
 
 cd test\Jellyfish.Commands.test
-dnx test 
+call dnx test 
+cd ../..
 if not "%errorlevel%"=="0" goto failure
-cd ..\..
 
 :success
 exit 0
